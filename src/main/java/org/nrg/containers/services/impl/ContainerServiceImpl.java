NrgXnat/container-service

commit 5fb72b0c6fac3c761ce16e5fce8ab844a73ab6e2

CS-688 Preparatory refactors (pull request #32)
* Add ContainerUtils.statusIsSuccessful

* Simplify ContainerEvent interface and handling to prepare for kubernetes events

* Add ContainerUtils.statusIsTerminal and TerminalState enum


Approved-by: Kate Alpert

src/main/java/org/nrg/containers/services/impl/ContainerServiceImpl.java
@@ -13,7 +13,6 @@ import org.mandas.docker.client.messages.swarm.TaskStatus;
 import org.nrg.action.ClientException;
 import org.nrg.containers.api.ContainerControlApi;
 import org.nrg.containers.events.model.ContainerEvent;
-import org.nrg.containers.events.model.DockerContainerEvent;
 import org.nrg.containers.events.model.ServiceTaskEvent;
 import org.nrg.containers.exceptions.CommandResolutionException;
 import org.nrg.containers.exceptions.ContainerBackendException;
@@ -801,42 +800,45 @@ public class ContainerServiceImpl implements ContainerService {
 
     @Override
     public void processEvent(final ContainerEvent event) {
-        log.debug("Processing container event");
+        log.debug("Processing container event: {}", event);
         final Container container = retrieve(event.containerId());
 
         // container will be null if either we aren't tracking the container
         // that this event is about, or if we have already recorded the event
-        if (container != null) {
-            if (isFinalizing(container)) {
-                log.debug("Container {} finalizing, skipping addl event {}", container.containerOrServiceId(), event);
-                return;
-            }
-
-            final String userLogin = container.userId();
-            try {
-                final UserI userI = Users.getUser(userLogin);
-
-                Container containerWithAddedEvent = addContainerEventToHistory(event, userI);
-                if (containerWithAddedEvent == null) {
-                    // Ignore this issue?
-                    containerWithAddedEvent = container;
-                }
-                if (event.isExitStatus()) {
-                    log.debug("Container is dead. Finalizing.");
-                    String status = containerWithAddedEvent.status();
-
-                    // If we don't have a status, assume success
-                    queueFinalize(event.exitCode(), status == null || DockerContainerEvent.isSuccessfulStatus(status),
-                            containerWithAddedEvent, userI);
-                }
-            } catch (UserInitException | UserNotFoundException e) {
-                log.error("Could not update container status. Could not get user details for user " + userLogin, e);
-            }
-        } else {
+        if (container == null) {
             log.debug("Nothing to do. Container was null after retrieving by id {}.", event.containerId());
+            return;
         }
 
-        log.debug("Done processing docker container event: {}", event);
+        if (isFinalizing(container)) {
+            log.debug("Container {} finalizing, skipping addl event {}", container.containerOrServiceId(), event);
+            return;
+        }
+
+        final String userLogin = container.userId();
+        final UserI userI;
+        try {
+            userI = Users.getUser(userLogin);
+        } catch (UserInitException | UserNotFoundException e) {
+            log.error("Could not get user details for user {}. Done processing container event: {}", userLogin, event, e);
+            return;
+        }
+
+        Container containerWithAddedEvent = addContainerEventToHistory(event, userI);
+        if (containerWithAddedEvent == null) {
+            // Ignore this issue?
+            containerWithAddedEvent = container;
+        }
+
+        if (event.isExitStatus()) {
+            log.debug("Container is dead. Finalizing.");
+
+            // If we don't have a status, assume success
+            queueFinalize(event.exitCode(), containerWithAddedEvent.status() == null,
+                    containerWithAddedEvent, userI);
+        }
+
+        log.debug("Done processing container event: {}", event);
     }
 
     @Override
@@ -1197,9 +1199,7 @@ public class ContainerServiceImpl implements ContainerService {
             throws ContainerException {
         String status = container.lastHistoryStatus();
         boolean isSuccessfulStatus = status == null || status.equals(FINALIZING) ||
-                (container.isSwarmService() ?
-                    ServiceTask.isSuccessfulStatus(status) :
-                    DockerContainerEvent.isSuccessfulStatus(status));
+                ContainerUtils.statusIsSuccessful(status, container.isSwarmService());
         finalize(container, userI, container.exitCode(), isSuccessfulStatus);
     }
 

src/main/java/org/nrg/containers/events/listeners/ContainerEventListener.java
@@ -1,7 +1,7 @@
 package org.nrg.containers.events.listeners;
 
 import lombok.extern.slf4j.Slf4j;
-import org.nrg.containers.events.model.DockerContainerEvent;
+import org.nrg.containers.events.model.ContainerEvent;
 import org.nrg.containers.services.ContainerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,17 +13,17 @@ import static reactor.bus.selector.Selectors.type;
 
 @Slf4j
 @Component
-public class DockerContainerEventListener implements Consumer<Event<DockerContainerEvent>> {
+public class ContainerEventListener implements Consumer<Event<ContainerEvent>> {
     private ContainerService containerService;
 
     @Autowired
-    public DockerContainerEventListener(final EventBus eventBus) {
-        eventBus.on(type(DockerContainerEvent.class), this);
+    public ContainerEventListener(final EventBus eventBus) {
+        eventBus.on(type(ContainerEvent.class), this);
     }
 
     @Override
-    public void accept(final Event<DockerContainerEvent> dockerContainerEventEvent) {
-        final DockerContainerEvent event = dockerContainerEventEvent.getData();
+    public void accept(final Event<ContainerEvent> containerEventEvent) {
+        final ContainerEvent event = containerEventEvent.getData();
         try {
             containerService.processEvent(event);
         } catch (Throwable e) {

src/main/java/org/nrg/containers/events/model/ContainerEvent.java
@@ -2,13 +2,12 @@ package org.nrg.containers.events.model;
 
 import org.nrg.framework.event.EventI;
 
-import java.util.Date;
 import java.util.Map;
 
 public interface ContainerEvent extends EventI {
     String containerId();
     String status();
-    Date time();
+    String externalTimestamp();
     Map<String, String> attributes();
     boolean isExitStatus();
     String exitCode();

src/main/java/org/nrg/containers/events/model/DockerContainerEvent.java
@@ -3,7 +3,6 @@ package org.nrg.containers.events.model;
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableMap;
 
-import javax.annotation.Nullable;
 import java.util.Date;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -15,8 +14,7 @@ public abstract class DockerContainerEvent implements ContainerEvent {
 
     public abstract String status();
     public abstract String containerId();
-    public abstract Date time();
-    @Nullable public abstract Long timeNano();
+    public abstract String externalTimestamp();
     public abstract ImmutableMap<String, String> attributes();
 
     public boolean isIgnoreStatus() {
@@ -30,16 +28,9 @@ public abstract class DockerContainerEvent implements ContainerEvent {
         return status != null && exitStatusPattern.matcher(status).matches();
     }
 
-    public static boolean isSuccessfulStatus(String status){
-        // No way to determine - always exits with "die", have to use exit code
-        return true;
-    }
-
     public String exitCode() {
         return isExitStatus() ?
-                (attributes().containsKey("exitCode") ?
-                        attributes().get("exitCode") :
-                        "") :
+                (attributes().getOrDefault("exitCode", "")) :
                 null;
     }
 
@@ -51,6 +42,7 @@ public abstract class DockerContainerEvent implements ContainerEvent {
         final ImmutableMap<String, String> attributesCopy = attributes == null ?
                 ImmutableMap.<String, String>of() :
                 ImmutableMap.copyOf(attributes);
-        return new AutoValue_DockerContainerEvent(status, containerId, time, timeNano, attributesCopy);
+        final String externalTimestamp = timeNano != null ? String.valueOf(timeNano) : time.toInstant().toString();
+        return new AutoValue_DockerContainerEvent(status, containerId, externalTimestamp, attributesCopy);
     }
 }

src/main/java/org/nrg/containers/model/container/auto/Container.java
@@ -15,7 +15,6 @@ import com.google.common.collect.Ordering;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.nrg.containers.events.model.ContainerEvent;
-import org.nrg.containers.events.model.DockerContainerEvent;
 import org.nrg.containers.exceptions.ContainerException;
 import org.nrg.containers.model.command.auto.ResolvedCommand;
 import org.nrg.containers.model.command.auto.ResolvedCommandMount;
@@ -26,6 +25,7 @@ import org.nrg.containers.model.container.entity.ContainerEntityInput;
 import org.nrg.containers.model.container.entity.ContainerEntityMount;
 import org.nrg.containers.model.container.entity.ContainerEntityOutput;
 import org.nrg.containers.model.container.entity.ContainerMountFilesEntity;
+import org.nrg.containers.utils.ContainerUtils;
 import org.nrg.containers.utils.JsonDateSerializer;
 import org.nrg.containers.utils.JsonStringToDateSerializer;
 import org.nrg.xft.event.persist.PersistentWorkflowI;
@@ -252,15 +252,7 @@ public abstract class Container {
 
     @JsonIgnore
     public boolean statusIsTerminal() {
-        final String status = status();
-        if (status != null) {
-            for (final String terminalStatus : ContainerEntity.TERMINAL_STATI) {
-                if (status.startsWith(terminalStatus)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return ContainerUtils.statusIsTerminal(status());
     }
 
 
@@ -1097,7 +1089,7 @@ public abstract class Container {
                     .entityType("event")
                     .entityId(null)
                     .timeRecorded(new Date())
-                    .externalTimestamp(containerEvent instanceof DockerContainerEvent ? String.valueOf(((DockerContainerEvent)containerEvent).timeNano()) : null)
+                    .externalTimestamp(containerEvent.externalTimestamp())
                     .message(null)
                     .exitCode(containerEvent.exitCode())
                     .build();

src/main/java/org/nrg/containers/model/container/entity/ContainerEntity.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.nrg.containers.model.container.auto.Container;
 import org.nrg.containers.services.impl.ContainerServiceImpl;
+import org.nrg.containers.utils.ContainerUtils;
 import org.nrg.framework.orm.hibernate.AbstractHibernateEntity;
 import org.nrg.xft.event.persist.PersistentWorkflowUtils;
 
@@ -16,24 +17,17 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Transient;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Entity
 @Slf4j
 public class ContainerEntity extends AbstractHibernateEntity {
     public static final String KILL_STATUS = "kill";
-    public static final Set<String> TERMINAL_STATI = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
-            "Complete", "Failed", "Killed"
-    )));
 
     private long commandId;
     private long wrapperId;
@@ -194,16 +188,13 @@ public class ContainerEntity extends AbstractHibernateEntity {
         return swarm ? ContainerServiceImpl.WAITING + " (" +inStatus+ ")" : inStatus;
     }
 
+    /**
+     * @deprecated Pass {@link ContainerEntity#getStatus()} to {@link ContainerUtils#statusIsTerminal(String)}
+     */
     @Transient
+    @Deprecated
     public boolean statusIsTerminal() {
-        if (status != null) {
-            for (final String terminalStatus : TERMINAL_STATI) {
-                if (status.startsWith(terminalStatus)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return ContainerUtils.statusIsTerminal(this.status);
     }
 
     public Date getStatusTime() {

src/main/java/org/nrg/containers/model/container/entity/ContainerEntityHistory.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.MoreObjects;
 import lombok.extern.slf4j.Slf4j;
 import org.nrg.containers.events.model.ContainerEvent;
-import org.nrg.containers.events.model.DockerContainerEvent;
 import org.nrg.containers.model.container.auto.Container;
 
 import javax.persistence.Column;
@@ -39,9 +38,7 @@ public class ContainerEntityHistory {
         history.entityId = null;
         history.timeRecorded = new Date();
         history.exitCode = containerEvent.exitCode();
-        if (containerEvent instanceof DockerContainerEvent) {
-            history.externalTimestamp = String.valueOf(((DockerContainerEvent)containerEvent).timeNano());
-        }
+        history.externalTimestamp = containerEvent.externalTimestamp();
         history.containerEntity = parent;
         return history;
     }

src/main/java/org/nrg/containers/services/impl/HibernateContainerEntityService.java
@@ -170,7 +170,10 @@ public class HibernateContainerEntityService
                         (containerEntity.getStatusTime() == null ||
                                 history.getTimeRecorded().getTime() > containerEntity.getStatusTime().getTime());
 
-        if (historyEntryIsMoreRecentThanContainerStatus && !containerEntity.statusIsTerminal()) {
+        if (historyEntryIsMoreRecentThanContainerStatus &&
+                (!ContainerUtils.statusIsTerminal(containerEntity.getStatus()) ||  // Don't overwrite a terminal status
+                        ContainerUtils.statusIsTerminal(history.getStatus()))  // ...except with a newer terminal status (i.e. Complete -> Failed)
+        ) {
             containerEntity.setStatusTime(history.getTimeRecorded());
             containerEntity.setStatus(history.getStatus());
             log.debug("Setting container entity {} status to \"{}\", based on history entry status \"{}\".",

commit 9a7e99fd5614065584159cb419bf4d0f7e567206

CS-688 Fix regression introduced in 5fb72b0c

src/main/java/org/nrg/containers/services/impl/ContainerServiceImpl.java
@@ -833,9 +833,8 @@ public class ContainerServiceImpl implements ContainerService {
         if (event.isExitStatus()) {
             log.debug("Container is dead. Finalizing.");
 
-            // If we don't have a status, assume success
-            queueFinalize(event.exitCode(), containerWithAddedEvent.status() == null,
-                    containerWithAddedEvent, userI);
+            // Docker containers always end with status "die". Have to determine success/failure from exit code.
+            queueFinalize(event.exitCode(), true, containerWithAddedEvent, userI);
         }
 
         log.debug("Done processing container event: {}", event);