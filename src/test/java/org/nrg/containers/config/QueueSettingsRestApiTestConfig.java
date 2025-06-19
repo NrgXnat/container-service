37e1d74c3809abf7e47e0604f7c4800b7b510955
James Ransford <james@embarklabs.ai>
2025-06-19 15:24:52 -0500

    CS-1031 Updating QueueSettingsRestApiTest

--- src/test/java/org/nrg/containers/config/QueueSettingsRestApiTestConfig.java
+++ src/test/java/org/nrg/containers/config/QueueSettingsRestApiTestConfig.java
@@ -2,31 +2,26 @@ package org.nrg.containers.config;
 
 import org.mockito.Mockito;
 import org.nrg.containers.jms.preferences.QueuePrefsBean;
-import org.nrg.containers.model.xnat.FakePrefsService;
 import org.nrg.containers.jms.rest.QueueSettingsRestApi;
+import org.nrg.containers.model.xnat.FakePrefsService;
 import org.nrg.containers.rest.QueueSettingsRestApiTest;
 import org.nrg.framework.services.ContextService;
 import org.nrg.framework.services.SerializerService;
 import org.nrg.prefs.services.NrgPreferenceService;
-import org.nrg.xdat.preferences.NotificationsPreferences;
 import org.nrg.xdat.security.services.PermissionsServiceI;
 import org.nrg.xdat.security.services.RoleHolder;
 import org.nrg.xdat.security.services.UserManagementServiceI;
 import org.nrg.xdat.services.AliasTokenService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerEndpointRegistry;
 import org.springframework.security.authentication.TestingAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import javax.jms.ConnectionFactory;
 
 @Configuration
 @EnableWebMvc
@@ -60,28 +55,8 @@ public class QueueSettingsRestApiTestConfig extends WebSecurityConfigurerAdapter
 
     @Bean
     public NrgPreferenceService fakePrefsService() {
-        return new FakePrefsService(QueueSettingsRestApiTest.TOOL_ID,
-                QueueSettingsRestApiTest.PREF_MAP);
-    }
-
-    private DefaultJmsListenerContainerFactory defaultMockFactory() {
-        DefaultJmsListenerContainerFactory factory = Mockito.spy(new DefaultJmsListenerContainerFactory());
-        ConnectionFactory connectionFactory = Mockito.mock(ConnectionFactory.class);
-        factory.setConnectionFactory(connectionFactory);
-        factory.setConcurrency(ContainersConfig.QUEUE_MIN_CONCURRENCY_DFLT + "-" + ContainersConfig.QUEUE_MAX_CONCURRENCY_DFLT);
-        return factory;
-    }
-
-    @Bean
-    @Qualifier("finalizingQueueListenerFactory")
-    public DefaultJmsListenerContainerFactory finalizingQueueListenerFactory() {
-        return defaultMockFactory();
-    }
-
-    @Bean
-    @Qualifier("stagingQueueListenerFactory")
-    public DefaultJmsListenerContainerFactory stagingQueueListenerFactory() {
-        return defaultMockFactory();
+        return new FakePrefsService(QueueSettingsRestApiTest.QUEUE_PREFS_BEAN_TOOL_ID,
+                QueueSettingsRestApiTest.DEFAULT_CONCURRENCY_PREFERENCES);
     }
 
     @Bean
